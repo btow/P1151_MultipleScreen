ff6740c6b81cf6a4bdf18f3549b053c51df9fbec
btow <btow@yandex.ru>
2017-05-10 22:15:38 +0500

    Step 2

--- app/src/main/java/com/example/samsung/p1151_multiplescreen/MainActivity.java
+++ app/src/main/java/com/example/samsung/p1151_multiplescreen/MainActivity.java
@@ -1,15 +1,16 @@
 package com.example.samsung.p1151_multiplescreen;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-import static com.example.samsung.p1151_multiplescreen.TitlesFragment.*;
+import static com.example.samsung.p1151_multiplescreen.TitlesFragment.onItemClickListener;
 
 public class MainActivity extends FragmentActivity implements onItemClickListener {
 
     private int position = 0;
+    private boolean withDetails = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,16 +20,25 @@ public class MainActivity extends FragmentActivity implements onItemClickListene
         if (savedInstanceState != null) {
             position = savedInstanceState.getInt("position");
         }
-        showDetails(position);
+        withDetails = (findViewById(R.id.cont) != null);
+
+        if (withDetails) {
+            showDetails(position);
+        }
     }
 
     private void showDetails(int position) {
-        DetailsFragment detailsFragment = (DetailsFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.cont);
-        if (detailsFragment == null || detailsFragment.getPosition() != position) {
-            detailsFragment = DetailsFragment.newInstance(position);
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.cont, detailsFragment).commit();
+
+        if (withDetails) {
+            DetailsFragment detailsFragment = (DetailsFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.cont);
+            if (detailsFragment == null || detailsFragment.getPosition() != position) {
+                detailsFragment = DetailsFragment.newInstance(position);
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.cont, detailsFragment).commit();
+            }
+        } else {
+            startActivity(new Intent(this, DetailsActivity.class).putExtra("position", position));
         }
     }
 
